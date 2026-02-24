2c8cb916cbdbed0422da91c2f29db04d75fc922c
Colin Hill <cjhill@pm.me>
2026-02-23 17:56:03 -0800

    Fix colors

--- dotfiles/.config/wezterm/colors/nord.lua
+++ dotfiles/.config/wezterm/colors/nord.lua
@@ -2,38 +2,37 @@ local wezterm = require 'wezterm'
 
 return {
   colors = {
-    foreground = "#DBDEE5",
-    background = "#181818",
+    foreground = '#ECEFF4',
+    background = '#121213',
 
-    cursor_bg = "#DBDEE5",
-    cursor_fg = "#181818",
-    cursor_border = "#DBDEE5",
+    cursor_bg = '#D8DEE9',
+    cursor_fg = '#2E3440',
+    cursor_border = '#D8DEE9',
 
-    selection_fg = "#181818",
-    selection_bg = "#6C717C",
-
-    scrollbar_thumb = "#42444A",
-    split = "#42444A",
+    selection_fg = '#ECEFF4',
+    selection_bg = '#434C5E',
 
     ansi = {
-        "#181818", -- black
-        "#B36C72", -- red
-        "#A3B495", -- green
-        "#E4C891", -- yellow
-        "#6A829F", -- blue
-        "#AA97A7", -- purple
-        "#97B3B2", -- teal
-        "#DBDEE5", -- darkest_white
+      '#07080A',
+      '#B48EAD',
+      '#779ecd',
+      '#8FBCBB',
+      '#5E81AC',
+      '#82b97c',
+      '#8FBCBB',
+      '#E5E9F0',
     },
+
     brights = {
-        "#6C717C", -- light_gray_bright
-        "#B36C72", -- red (bright)
-        "#A3B495", -- green (bright)
-        "#E4C891", -- yellow (bright)
-        "#90BBC7", -- off_blue
-        "#AA97A7", -- purple (bright)
-        "#8AA0B7", -- glacier
-        "#EDEFF2", -- white
+      '#4C566A',
+      '#BF616A',
+      '#A3BE8C',
+      '#EBCB8B',
+      '#5E81AC',
+      '#B48EAD',
+      '#8FBCBB',
+      '#ECEFF4',
     },
   },
 }
+
--- dotfiles/.local/share/nvim/site/pack/core/opt/nord.nvim/lua/nord/named_colors.lua
+++ dotfiles/.local/share/nvim/site/pack/core/opt/nord.nvim/lua/nord/named_colors.lua
@@ -1,22 +1,22 @@
 local nord = {
 	--16 colors
-	black = "#181818", -- nord0 in palette
-	dark_gray = "#42444A", -- nord1 in palette
-	gray = "#4B4E55", -- nord2 in palette
-	light_gray = "#555960", -- nord3 in palette
-	light_gray_bright = "#6C717C", -- out of palette
-	darkest_white = "#DBDEE5", -- nord4 in palette
-	darker_white = "#E7E9ED", -- nord5 in palette
-	white = "#EDEFF2", -- nord6 in palette
-	teal = "#97B3B2", -- nord7 in palette
-	off_blue = "#90BBC7", -- nord8 in palette
-	glacier = "#8AA0B7", -- nord9 in palette
-	blue = "#6A829F", -- nord10 in palette
-	red = "#B36C72", -- nord11 in palette
-	orange = "#C68B79", -- nord12 in palette
-	yellow = "#E4C891", -- nord13 in palette
-	green = "#A3B495", -- nord14 in palette
-	purple = "#AA97A7", -- nord15 in palette
+	black = "#07080A",
+	dark_gray = "#3B4252",
+	gray = "#434C5E",
+	light_gray = "#4C566A",
+	light_gray_bright = "#616E88",
+	darkest_white = "#D8DEE9",
+	darker_white = "#E5E9F0",
+	white = "#E5E9F0",
+	teal = "#8FBCBB",
+	off_blue = "#88C0D0",
+	glacier = "#81A1C1",
+	blue = "#5E81AC",
+	red = "#B48EAD",
+	orange = "#D08770",
+	yellow = "#8FBCBB",
+	green = "#738eb0",
+	purple = "#B48EAD",
 	none = "NONE",
 }
 return nord
